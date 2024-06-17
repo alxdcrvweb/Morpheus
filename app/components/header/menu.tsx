21b2afea1550f0820c87d742742396d7ccd0db19
alexweb3dev0x <alexweb3dev0x@proton.me>
2024-06-17 12:50:52 +0200

    header menu as array

--- app/components/header/menu.tsx
+++ app/components/header/menu.tsx
@@ -1,95 +1,52 @@
 import cn from "classnames";
-// import { useInjection } from "inversify-react";
-// import { observer } from "mobx-react";
-import { FC, useEffect, useState } from "react";
-import styles from "../../styles/header.module.scss";
-// import { maskAddress } from "../../utils/utilities";
-// import Social from "../Social";
-// import eye from "../../public/icons/eye.svg";
-// import Image from "next/image";
-// import { useRouter } from "next/router";
+import { FC } from "react";
 import Link from "next/link";
-// import { Web3Store } from "../../stores/Web3Store";
+import { usePathname } from "next/navigation";
 
-interface HeaderProps {
-    routerPath: string;
-    active: string;
-}
-
-const Menu: FC<HeaderProps> = () => {
-    // const router = useRouter();
-    // const web3Store = useInjection<Web3Store>(Web3Store);
-
-    return (
-        <div className={"header__actions"}>
-            {
-                <Link href="/">
-                    <div
-                        className={cn(
-                            "header__menu",
-                            "header__active",
-                            "first"
-                        )}
-                    >
-                        Journey
-                    </div>
-                </Link>
-            }
-            {
-                <Link href="/story">
-                    <div
-                        className={cn(
-                           "header__menu",
-                            // active == "story" && styles.header__active
-                        )}
-                    >
-                        Story
-                    </div>
-                </Link>
-            }
-            {
-                <Link href="/sleeping">
-                    <div
-                        className={cn(
-                             "header__menu",
-                            // active == "active" && styles.header__active
-                        )}
-                    >
-                        Factions
-                    </div>
-                </Link>
-            }
-            {
-                <Link href="/gallery">
-                    <div
-                        className={cn(
-                            "header__menu",
-                            // active == "gallery" && styles.header__active,
-                            // !web3Store.farcasterUser &&
-                            // !web3Store.address &&
-                            "header__disabled"
-                        )}
-                    >
-                        Gallery
-                    </div>
-                </Link>
-            }
-            {
-                <Link href="/leaderboard">
-                    <div
-                        className={cn(
-                            "header__menu",
-
-                            "last",
-                            // active == "leaderboard" && styles.header__active
-                        )}
-                    >
-                        Leaderboard
-                    </div>
-                </Link>
-            }
-        </div>
-    );
-}
+const links = [
+  {
+    title: "Journey",
+    link: "/",
+  },
+  {
+    title: "Story",
+    link: "/story",
+  },
+  {
+    title: "Factions",
+    link: "/sleeping",
+  },
+  {
+    title: "Gallery",
+    link: "/gallery",
+    loginRequired: true,
+  },
+  {
+    title: "Leaderboard",
+    link: "/leaderboard",
+  },
+];
+const Menu: FC = () => {
+  const pathname = usePathname();
+  return (
+    <div className={"header__actions"}>
+      {links.map((row, i) => (
+        <Link href={row.link} key={i}>
+          <div
+            className={cn(
+              "header__menu",
+              pathname == row.link && "header__active",
+              i == 0 && "first",
+              i == links.length - 1 && "last",
+              row.loginRequired && "header__disabled"
+            )}
+          >
+            {row.title}
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
 
 export default Menu;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,27 @@
-import "../styles/globals.scss"
-import "../styles/layout.scss"
+import "../styles/globals.scss";
+import "../styles/layout.scss";
 import Image from "next/image";
 import background from "@/public/bg.png";
 import background2 from "@/public/bg_2.png";
 import Header from "@/app/components/header";
 
-export default function RootLayout({children}: {
-    children: React.ReactNode
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body>
+  return (
+    <html lang="en">
+      <body>
+        {/*background*/}
+        <div className="container">
+          <Image className={"bg"} src={background} fill={true} />
+          <Image className={"bg"} src={background2} fill={true} />
+        </div>
+        <Header />
 
-            {/*background*/}
-            <div className="container">
-                <Image
-                    className={"bg"}
-                    src={background}
-                    fill={true}
-                />
-            <Image
-                    className={"bg"}
-                    src={background2}
-                    fill={true}
-                />
-            </div>
-            <Header />
-
-            <main>{children}</main>
-            </body>
-        </html>
-    )
+        <main>{children}</main>
+      </body>
+    </html>
+  );
 }
