ec618d32ce5d559d3e594413f7480ed081a96484
alexweb3dev0x <alexweb3dev0x@proton.me>
2024-08-13 22:44:37 +0200

    bg

--- app/components/background/index.tsx
+++ app/components/background/index.tsx
@@ -1,13 +1,27 @@
 "use client";
 import "@/styles/globals.scss";
 import "@/styles/layout.scss";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const Index = () => {
+  const pname = usePathname();
+  const [path, setPath] = useState("");
+  useEffect(() => {
+    setPath(pname.replaceAll("/", ""));
+  }, [pname]);
   return (
     <div className="bg">
-      <video preload="auto" loop autoPlay muted playsInline className="video">
-        <source src="/background/bg_video.mp4" type="video/mp4"></source>
-      </video>
+      {
+        <video preload="auto" loop autoPlay muted playsInline className="video" style={{opacity: path !== "showdown" ? 1 : 0}}>
+          <source src="/background/bg_video.mp4" type="video/mp4"></source>
+        </video>
+      }
+      {
+        <video preload="auto" loop autoPlay muted playsInline className="video2" style={{opacity: path == "showdown" ? 1 : 0}}>
+          <source src="/background/map.mp4" type="video/mp4"></source>
+        </video>
+      }
     </div>
   );
 };
